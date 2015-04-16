ad88fe7fba1ba2d52791f4b1cb2b39d806c1e548
david s <david.salinas@msgg.gob.cl>
2015-04-16 16:27:33 -0300

    Corrige import a normalize.less

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>
         <?php
